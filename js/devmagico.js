5d1514195e447800d0fdaca8ee71cba9e6ab7d7a
Alexsandro Cristiano <acg.alexsandro@hotmail.com>
2022-01-20 09:16:47 -0300

    realiza get na API e renderiza na pagina

--- js/devmagico.js
+++ js/devmagico.js
@@ -1,16 +1,3 @@
-/*
-<div class="dm-card-container">
-        <img
-          src="http://hp-api.herokuapp.com/images/hermione.jpeg"
-          class=
-          alt=""
-        />
-        <div class=>
-          <h5 class=>nome do personagem</h5>
-        </div>
-      </div>
- */
-
 async function getJson(url) {
   try {
     const response = await fetch(url);
@@ -21,26 +8,37 @@ async function getJson(url) {
   }
 }
 
-function criarCartao(infoPersonagem) {
-    const divContainer = document.createElement('div')
-    const img = document.createElement('img')
-    const divBody = document.createElement('div')
-    const h5 = document.createElement('h5')
+function criarCartao(p) {
+  const divContainer = document.createElement("div");
+  const img = document.createElement("img");
+  const divBody = document.createElement("div");
+  const h5 = document.createElement("h5");
 
-    divContainer.classList.add("dm-card-container")
-    img.classList.add("rounded-circle")
-    divBody.classList.add("card-body")
-    h5.classList.add("card-title")
-    
-    img.setAttribute('src', "http://hp-api.herokuapp.com/images/lupin.jpg")
-    h5.innerHTML = 'deu certo'
+  divContainer.classList.add("dm-card-container");
+  img.classList.add("rounded-circle");
+  divBody.classList.add("card-body");
+  h5.classList.add("card-title");
 
-    divBody.appendChild(h5)
-    divContainer.appendChild(img)
-    divContainer.appendChild(divBody)
-    return divContainer
+  img.setAttribute("src", `${p.image ?? null}`);
+  h5.innerHTML = p.name;
+
+  divBody.appendChild(h5);
+  divContainer.appendChild(img);
+  divContainer.appendChild(divBody);
+  return divContainer;
 }
-console.log(criarCartao())
 
-const root = document.querySelector('#root')
-root.appendChild(criarCartao())
+async function init() {
+  const url = "http://hp-api.herokuapp.com/api/characters";
+  const personagem = await getJson(url);
+
+  console.log(personagem)
+
+  const root = document.querySelector("#root");
+
+  personagem.forEach((element) => {
+    root.appendChild(criarCartao(element));
+  });
+}
+
+init()
