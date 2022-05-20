449a53e8190f31fce07958566fa44699e3359ac9
alexsandro-cristiano <acg.alexsandro@hotmail.com>
2022-05-19 21:01:32 -0300

    rederiza os dados do livro na tela sem formatacao

--- assets/js/devmagico.js
+++ assets/js/devmagico.js
@@ -12,18 +12,9 @@ function modifySideBar(sidebarArrow) {
   sidebarArrow.classList.toggle('icon-arrow-right')
   sidebarArrow.classList.toggle('icon-arrow-left')
 
-  for (const element of itemList) {
-    element.classList.toggle('nav__item__title--open')
+  for (const item of itemList) {
+    item.classList.toggle('nav__item__title--open')
   }
-}
-
-async function getInformation(url) {
-  fetch(url)
-    .then(res => res.json())
-    .then(data => {
-      //o que fazer aqui ?
-    })
-    .catch(e => console.log(e))
 }
 
 /**
@@ -36,12 +27,12 @@ function naosei() {
               <div class="card__image__container">
                 <img
                   src=""
-                  alt="TESTE"
+                  alt=""
                   class="card__image"
                 />
               </div>
               <div class="card__info card__info--active">
-                <h3 class="card__info__title">Teste 1</h3>
+                <h3 class="card__info__title"></h3>
                 <i class="card__info__icon icon-arrow-down"></i>
               </div>
             </div>
@@ -50,25 +41,61 @@ function naosei() {
   const mainUl = document.getElementsByClassName('main__list')
 }
 
-function switchOption(event) {
+async function getInformation(url) {
+  const response = await fetch(url)
+  const jsonBody = await response.json()
+  return jsonBody
+}
+
+async function switchOption(event) {
   let url = 'https://fedeperin-harry-potter-api.herokuapp.com/'
-  let information
+  let arrayteste = []
   switch (event) {
     case 'livro':
       url += 'libros'
-      getInformation(url)
+      arrayteste = await getInformation(url)
+
+      console.log('\ntamanho do array = ' + `${arrayteste.length}`)
+
+      arrayteste.forEach((element) => {
+        console.log('bla')
+        const html = `
+          <li class="main__item">
+            <div class="card">
+              <div class="card__info card__info--active">
+                <h3 class="card__info__title">${element.titulo_original}</h3>
+                <h3 class="card__info__title">${element.autora}</h3>
+                <p class="card__info__title">${element.descripcion}</p>
+                <i class="card__info__icon icon-arrow-down"></i>
+              </div>
+            </div>
+          </li>
+  `
+        const mainUl = document.querySelector('.main__list')
+        console.log(mainUl)
+        mainUl.insertAdjacentHTML('beforeend', html)
+      })
+      /**
+       * estou fazendo a requisição
+       * estou guardando a resposta que é um vetor na variavel
+       * estou varrendo a variavel e exibindo no console o nome do livro
+       */
+
       break
     case 'personagem':
       url += 'personajes'
-      getInformation(url)
+      console.log('\n estou na funcao\n\n\n')
+      console.log(await getInformation(url))
       break
     case 'fentico':
       url += 'hechizos'
-      getInformation(url)
+      console.log('\n estou na funcao\n\n\n')
+      console.log(await getInformation(url))
       break
     case 'funcionario':
       url = 'http://hp-api.herokuapp.com/api/characters/staff'
-      getInformation(url)
+      console.log('\n estou na funcao\n\n\n')
+      console.log(await getInformation(url))
       break
   }
 }
@@ -80,7 +107,7 @@ function init() {
   })
 
   const menuList = document.getElementById('list')
-  menuList.addEventListener('click', event => {
+  menuList.addEventListener('click', (event) => {
     switchOption(event.target.id)
   })
 }
