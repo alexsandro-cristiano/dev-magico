d255cde50c005851bd79dd6275e9ab46ee48dbe6
alexsandro-cristiano <acg.alexsandro@hotmail.com>
2022-06-27 13:42:59 -0300

    cards de livros ok

--- assets/js/card.js
+++ assets/js/card.js
@@ -1,26 +1,26 @@
-const ulMainCard = document.querySelector('.main .list')
-
-export function renderLibros(element) {
-  console.log('cheguei ate a funda renderLibros')
+export function renderLibros(element, ulMainCard) {
   const html = `
   <li class="item">
            <div class="card">
             <div class="cardHeader">
-              <i class="icon icon-book"></i>
+              <i class="icon-book"></i>
             </div>
             <div class="cardBody">
-            <h3 class="card__info__title">
+            <div class="cardTitle">
             ${element.libro}
-            </h3>
-              <h3 class="card__info__title">${element.autora}</h3>
-              <p class="card__info__title">${element.descripcion}</p>
-              
+            </div>
+            <p class="card__info__title">${element.descripcion}</p>
+            
             </div>
             <div class="cardFooter">
-              <i class="card__info__icon icon-arrow-down"></i>
+             <h3 class="card__info__title">${element.autora}</h3>
             </div>
            </div>
           </li>
   `
   ulMainCard.insertAdjacentHTML('beforeend', html)
+}
+
+export function renderPersonagem(element, ulMainCard) {
+  console.log(element)
 }
--- assets/js/devmagico.js
+++ assets/js/devmagico.js
@@ -1,4 +1,89 @@
-import * as sidebar from './sidebar.js'
+import * as request from './request.js'
+import * as renderCard from './card.js'
+const ulMainCard = document.querySelector('.main .list')
+let antigo = ''
+
+function modificarLogo() {
+  const logo = document.querySelector('.logo__title')
+  logo.classList.toggle('logo__title--open')
+}
+
+function expandSidebar(sidebarArrow) {
+  modificarLogo()
+  const sidebar = document.querySelector('.sidebar')
+  const itemList = document.querySelectorAll('.nav__item__title')
+
+  sidebar.classList.toggle('sidebar--open')
+  sidebarArrow.classList.toggle('icon-arrow-right')
+  sidebarArrow.classList.toggle('icon-arrow-left')
+
+  for (const item of itemList) {
+    item.classList.toggle('nav__item__title--open')
+  }
+}
+
+async function switchOption(option) {
+  let url = 'https://fedeperin-harry-potter-api.herokuapp.com/'
+  let informations = []
+  activateMenuAtCurrentItem(option)
+  switch (option) {
+    case 'livro':
+      url += 'libros'
+      informations = await request.getJson(url)
+      ulMainCard.innerHTML = ''
+      for (const inf of informations) {
+        renderCard.renderLibros(inf, ulMainCard)
+      }
+      break
+    case 'personagem':
+      url += 'personajes'
+      informations = await request.getJson(url)
+      ulMainCard.innerHTML = ''
+      for (const inf of informations) {
+        renderCard.renderPersonagem(inf, ulMainCard)
+      }
+      break
+    case 'fentico':
+      url += 'hechizos'
+      informations = await request.getJson(url)
+      ulMainCard.innerHTML = ''
+      for (const inf of informations) {
+        renderCard.renderLibros(inf, ulMainCard)
+      }
+      break
+    case 'funcionario':
+      url = 'http://hp-api.herokuapp.com/api/characters/staff'
+      informations = await request.getJson(url)
+      ulMainCard.innerHTML = ''
+      for (const inf of informations) {
+        renderCard.renderLibros(inf, ulMainCard)
+      }
+      break
+  }
+}
+
+function activateMenuAtCurrentItem(novo) {
+  document.getElementById(`${novo}`).classList.add('active')
+  if (antigo !== '') {
+    document.getElementById(`${antigo}`).classList.remove('active')
+  }
+  antigo = novo
+}
+
+function init() {
+  const sidebarArrow = document.querySelector('.js-icon')
+  const menuList = document.getElementById('list')
+
+  sidebarArrow.addEventListener('click', () => {
+    expandSidebar(sidebarArrow)
+  })
+  menuList.addEventListener('click', event => {
+    switchOption(event.target.id)
+  })
+}
+
+init()
+window.onload = switchOption('livro')
 
 /*
 function openInformation(cardArrow) {
@@ -14,4 +99,3 @@ function init() {
   })
 }
 */
-sidebar.init()
